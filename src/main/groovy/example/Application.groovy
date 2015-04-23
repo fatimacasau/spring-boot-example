7d3ec9176bb0fea636fcbd83619e8bfa5fcc12ad
Fatima Casau <fcasau@paradigmatecnologico.com>
2015-04-23 16:43:03 +0200

    first commit

---
+++ src/main/groovy/example/Application.groovy
@@ -0,0 +1,14 @@
+package example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ApplicationContext
+
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args)
+    }
+
+}
